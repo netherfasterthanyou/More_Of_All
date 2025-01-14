db7b80408e26dfd1b71b8f97932c783887f576e1
netherfasterthanyou <adityakkaipa@gmail.com>
2025-01-13 19:56:06 -0800

    added new Items CHISELS! They can be used one their specific block to be changed to a different varient like prismarine to prismarine bricks! Added the SALVAGER drop a valid item on the salvager like a netherite sword to salvage it into a netherite ingot! I feel like it should be in vanilla rather than smelting down an iron chestplate just to get 1 iron nugget. Added The ChainSaw! Instantly mine through leaves and logs you need to right click to use

--- src/main/java/com/mod/more_of_all/ExampleMod.java
+++ src/main/java/com/mod/more_of_all/ExampleMod.java
@@ -18,6 +18,9 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
+
+
+
 @Mod(ExampleMod.MOD_ID)
 public class ExampleMod
 {
--- src/main/java/com/mod/more_of_all/item/FoodProperties.java
+++ src/main/java/com/mod/more_of_all/item/FoodProperties.java
@@ -5,7 +5,7 @@ import net.minecraft.world.effect.MobEffects;
 
 public class FoodProperties {
     public static final net.minecraft.world.food.FoodProperties CHILI = new net.minecraft.world.food.FoodProperties.Builder().nutrition(3).saturationModifier(0.25f)
-            .effect(new MobEffectInstance(MobEffects.LUCK, 4000), 0.1f)
-            .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 3000), 0.7f).build();
+            .effect(new MobEffectInstance(MobEffects.LUCK, 4000), 0.3f)
+            .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1500), 0.7f).build();
 
 }
